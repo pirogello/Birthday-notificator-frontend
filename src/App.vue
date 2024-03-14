4ec4e3d0e4d5efb9ffcd161d3bef8d147c9318da
NikitaPie <nikita-pirogov-artur@mail.ru>
2024-03-14 17:09:24 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <h1 class="title">Birthday notificator</h1>
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
